574ca9482ae2fd90ff266145bfe3bc211de3eb41
Adam Shields <adamenote@gmail.com>
2020-02-20 10:49:30 -0500

    used to string to convert a number into a string

--- home.js
+++ home.js
@@ -4,6 +4,10 @@
 // showMessage(message);
 
 
-let message = 'Hello';
-message = message.length;
-showMessage(message);
+// let message = 'Hello';
+// message = message.length;
+// showMessage(message);
+
+let amount = 123;
+amount = amount.toString();
+showMessage(typeof amount);
