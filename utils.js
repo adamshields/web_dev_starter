495ce365ba70387f6989a3a9af125ee5f0ef4149
Adam Shields <adamenote@gmail.com>
2020-02-19 20:30:57 -0500

    video 3.1 -- video 3.3

--- utils.js
+++ utils.js
@@ -1,3 +1,4 @@
+// Sample Function that gets element that I defined in index.html I defined showMessage in the script.js file
 function showMessage(message) {
     document.getElementById('message').textContent = message;
 }
