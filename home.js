abad73998fbbfac321f3872a1f4cb8d20b6c506f
Adam Shields <adamenote@gmail.com>
2020-02-20 10:51:46 -0500

    converts a string to a floating number using parse float

--- home.js
+++ home.js
@@ -8,6 +8,12 @@
 // message = message.length;
 // showMessage(message);
 
-let amount = 123;
+// let amount = 123;
+// amount = amount.toString();
+// showMessage(typeof amount);
+
+
+// converts a string to a floating number using parse float
+let amount = Number.parseFloat("123.12");
 amount = amount.toString();
-showMessage(typeof amount);
+showMessage(amount);
