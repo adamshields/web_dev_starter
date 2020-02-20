9937882cb7c6d3bdfdcd6a6a9bdb4b73aae1e2d0
Adam Shields <adamenote@gmail.com>
2020-02-20 10:11:09 -0500

    using constant vaiable

--- home.js
+++ home.js
@@ -3,14 +3,16 @@
 // let is for defining a variable 
 
 //  You can also define variables using let once and then separate them by commas and end with a semi colon ;
-let price;
+// let price;
+
+const price = 40;
 
 // lots of code
-price = 49.99;
+// price = 49.99;
 
 // more code
 
-price = 29.99;
+// price = 29.99;
 
 
 showMessage(price);
