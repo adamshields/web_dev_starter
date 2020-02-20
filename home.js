3cff75d7245a3d5e04e6805e385cf960efa36b2c
Adam Shields <adamenote@gmail.com>
2020-02-20 10:12:38 -0500

    normally start off with a constant variable instead of let you can always go back and change it later

--- home.js
+++ home.js
@@ -1,29 +1,9 @@
-// Strings can use single quotes or double quotes
-
-// let is for defining a variable 
-
-//  You can also define variables using let once and then separate them by commas and end with a semi colon ;
-// let price;
 
 const price = 40;
-
-// lots of code
-// price = 49.99;
-
-// more code
-
-// price = 29.99;
-
 
 showMessage(price);
 
 
 
-// Single Line Comment
 
-/*
-Multi 
-Line
-Comment
-*/
 
