ca8de586a2aaa054dc104977542eac216c6eeff5
Adam Shields <adamenote@gmail.com>
2020-02-20 10:00:45 -0500

    define variables multiple ways using let

--- home.js
+++ home.js
@@ -1,5 +1,14 @@
-showMessage("Title 1");
-showMessage("Title 2");
+// Strings can use single quotes or double quotes
+
+// let is for defining a variable 
+
+//  You can also define variables using let once and then separate them by commas and end with a semi colon ;
+let price = 49.99,
+    name = 'Hiking Boots',
+    discounted = false;
+
+showMessage(discounted);
+
 
 
 // Single Line Comment
@@ -9,3 +18,4 @@ Multi
 Line
 Comment
 */
+
